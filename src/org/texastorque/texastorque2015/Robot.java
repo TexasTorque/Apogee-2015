16a0c9141a9e626b918264d52f09f0ce309cdc50
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-01-23 17:36:03 -0600

    Implemented everything in main

--- src/org/texastorque/texastorque2015/Robot.java
+++ src/org/texastorque/texastorque2015/Robot.java
@@ -1,7 +1,55 @@
 package org.texastorque.texastorque2015;
 
+import org.texastorque.texastorque2015.feedback.Feedback;
+import org.texastorque.texastorque2015.input.DriverInput;
+import org.texastorque.texastorque2015.input.Input;
+import org.texastorque.texastorque2015.output.Output;
+import org.texastorque.texastorque2015.output.RobotOutput;
+import org.texastorque.texastorque2015.subsystem.Drivebase;
 import org.texastorque.torquelib.base.TorqueIterative;
 
 public class Robot extends TorqueIterative {
+    //Subsystems
+    Drivebase drivebase;
+    
+    //Input
+    Input activeInput;
+    DriverInput driverInput;
+    
+    //Output
+    Output activeOutput;
+    RobotOutput robotOutput;
+    
+    //Feedback
+    Feedback activeFeedback;
+
+    @Override
+    public void robotInit() {
+        drivebase = new Drivebase();
+        driverInput = new DriverInput();
+        robotOutput = new RobotOutput();
+    }
+
+    @Override
+    public void teleopInit() {
+        activeInput = driverInput;
+        activeOutput = robotOutput;
+        
+        drivebase.setInput(activeInput);
+        drivebase.setOutput(activeOutput);
+        
+        drivebase.loadParams();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        activeInput.run();
+        drivebase.pushToDashboard();
+    }
+
+    @Override
+    public void teleopContinuous() {
+        drivebase.run();
+    }
     
 }
