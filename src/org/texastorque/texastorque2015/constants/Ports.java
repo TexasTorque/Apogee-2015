316d3a584a137279b552e49a0fe2f4971b09151f
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-01-23 22:02:14 -0600

    Real ports

--- src/org/texastorque/texastorque2015/constants/Ports.java
+++ src/org/texastorque/texastorque2015/constants/Ports.java
@@ -3,8 +3,8 @@ package org.texastorque.texastorque2015.constants;
 public class Ports {
     public final static int LEFT_A_DRIVE_PORT = 0;
     public final static int LEFT_B_DRIVE_PORT = 1;
-    public final static int RIGHT_A_DRIVE_PORT = 2;
+    public final static int RIGHT_A_DRIVE_PORT = 4;
     public final static int RIGHT_B_DRIVE_PORT = 3;
-    public final static int FRONT_STRAFE_PORT = 4;
+    public final static int FRONT_STRAFE_PORT = 2;
     public final static int REAR_STRAFE_PORT = 5;
 }
