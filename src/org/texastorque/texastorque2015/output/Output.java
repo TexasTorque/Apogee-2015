e56f597ae0a1b38bfa4cb23775b48d6f2b9f0770
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-01-30 08:07:14 -0600

    More comments

--- src/org/texastorque/texastorque2015/output/Output.java
+++ src/org/texastorque/texastorque2015/output/Output.java
@@ -2,15 +2,19 @@ package org.texastorque.texastorque2015.output;
 
 public abstract class Output {
 
+    //Drivebase
     public abstract void setDriveSpeeds(double left, double right, double frontStrafe, double rearStrafe);
 
+    //Elevator
     public abstract void setElevatorMotorSpeeds(double speed);
 
+    //Crazy Arms
     public abstract void setArmsOpen(boolean open);
 
     public abstract void setTiltUp(boolean on);
 
     public abstract void setPunchOut(boolean out);
     
+    //Intake
     public abstract void setIntakeMotorSpeed(double speed);
 }
--- src/org/texastorque/texastorque2015/output/RobotOutput.java
+++ src/org/texastorque/texastorque2015/output/RobotOutput.java
@@ -21,6 +21,8 @@ public class RobotOutput extends Output {
     private Solenoid openSolenoid;
     private Solenoid punchSolenoid;
     private DoubleSolenoid tiltSolenoid;
+    
+    //Intake
     private TorqueMotor leftIntakeMotor;
     private TorqueMotor rightIntakeMotor;
 
@@ -37,6 +39,8 @@ public class RobotOutput extends Output {
         openSolenoid = new Solenoid(Ports.OPEN_SOLENOID_PORT);
         punchSolenoid = new Solenoid(Ports.PUNCH_SOLENOID);
         tiltSolenoid = new DoubleSolenoid(Ports.TILT_SOLENOID_FORWARD_PORT, Ports.TILT_SOLENOID_BACKWARD_PORT);
+        
+        //Intake
         leftIntakeMotor = new TorqueMotor(new VictorSP(Ports.LEFT_INTAKE_PORT), true, TorqueMotor.LinearizationType.kNone);
         rightIntakeMotor = new TorqueMotor(new VictorSP(Ports.RIGHT_INTAKE_PORT), false, TorqueMotor.LinearizationType.kNone);
     }
@@ -73,6 +77,7 @@ public class RobotOutput extends Output {
         punchSolenoid.set(out);
     }
 
+    //Intake
     @Override
     public void setIntakeMotorSpeed(double speed) {
         leftIntakeMotor.set(speed);
