5f61f498cbbb41300e543fdfaf5ad7e37dc56b7e
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-03-19 16:02:36 -0500

    Bayou ports

--- src/org/texastorque/texastorque2015/constants/Ports.java
+++ src/org/texastorque/texastorque2015/constants/Ports.java
@@ -3,10 +3,10 @@ package org.texastorque.texastorque2015.constants;
 public class Ports {
 
     //Drivebase motor ports
-    public final static int LEFT_A_DRIVE_PORT = 11;
+    public final static int LEFT_A_DRIVE_PORT = 11;//unused
     public final static int LEFT_B_DRIVE_PORT = 3;
-    public final static int RIGHT_A_DRIVE_PORT = 4;
-    public final static int RIGHT_B_DRIVE_PORT = 12;
+    public final static int RIGHT_A_DRIVE_PORT = 1;
+    public final static int RIGHT_B_DRIVE_PORT = 12;//unused
     public final static int STRAFE_PORT = 0;
 
     //Elevator
@@ -29,18 +29,18 @@ public class Ports {
     public final static int SLUICE_BUTTON = 5;
 
     //crazy arms
-    public final static int ARM_A = 3;
-    public final static int ARM_B = 5;
+    public final static int ARM_A = 5;
+    public final static int ARM_B = 1;
     public final static int PUNCH_SOLENOID = 6;
     public final static int INTAKE_SOLENOID = 4;
-    public final static int TILT_SOLENOID_A_PORT = 0;
-    public final static int TILT_SOLENOID_B_PORT = 7;
+    public final static int TILT_SOLENOID_A_PORT = 7;
+    public final static int TILT_SOLENOID_B_PORT = 0;
 
     //intake
-    public final static int LEFT_INTAKE_PORT = 6;
-    public final static int RIGHT_INTAKE_PORT = 9;//?
+    public final static int LEFT_INTAKE_PORT = 9;
+    public final static int RIGHT_INTAKE_PORT = 6;//?
 
     //gyro
-    public final static int GYRO_PORT_A = 0;
-    public final static int GYRO_PORT_B = 1;
+    public final static int GYRO_PORT_A = 1;
+    public final static int GYRO_PORT_B = 0;
 }
