20f7344c6c6145c854a57dd9640555eeff2999f1
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-03-20 07:27:56 -0500

    Encoder ports change

--- src/org/texastorque/texastorque2015/constants/Ports.java
+++ src/org/texastorque/texastorque2015/constants/Ports.java
@@ -14,12 +14,12 @@ public class Ports {
     public final static int RIGHT_ELEVATOR = 8;
 
     //encoders
-    public final static int RIGHT_ENCODER_PORT_A = 2;
-    public final static int RIGHT_ENCODER_PORT_B = 3;
+    public final static int RIGHT_ENCODER_PORT_A = 3;
+    public final static int RIGHT_ENCODER_PORT_B = 2;
     public final static int LEFT_ENCODER_PORT_A = 0;
     public final static int LEFT_ENCODER_PORT_B = 1;
-    public final static int LEFT_ELEVATOR_ENCODER_PORT_A = 7;
-    public final static int LEFT_ELEVATOR_ENCODER_PORT_B = 6;
+    public final static int LEFT_ELEVATOR_ENCODER_PORT_A = 6;
+    public final static int LEFT_ELEVATOR_ENCODER_PORT_B = 7;
     public final static int RIGHT_ELEVATOR_ENCODER_PORT_A = 8;
     public final static int RIGHT_ELEVATOR_ENCODER_PORT_B = 9;
 
