904a07c884e13e8eff38b18ce1648a581e2794c3
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-04-02 14:09:17 -0500

    Redid auto stacking

--- src/org/texastorque/texastorque2015/input/DriverInput.java
+++ src/org/texastorque/texastorque2015/input/DriverInput.java
@@ -1,5 +1,6 @@
 package org.texastorque.texastorque2015.input;
 
+import org.texastorque.texastorque2015.constants.Constants;
 import org.texastorque.torquelib.util.GenericController;
 import org.texastorque.torquelib.util.TorqueFilter;
 
@@ -7,13 +8,18 @@ public class DriverInput extends Input {
 
     private GenericController driver;
     private GenericController operator;
+    private OperatorConsole panel;
 
     private TorqueFilter driveAccelFilter;
     private TorqueFilter turnAccelFilter;
 
+    private boolean wentDown;
+    private boolean isAutoStack;
+
     public DriverInput() {
         driver = new GenericController(0, GenericController.TYPE_XBOX, 0.2);
         operator = new GenericController(1, GenericController.TYPE_XBOX, 0.2);
+        panel = new OperatorConsole(2);
 
         driveAccelFilter = new TorqueFilter(25);
         turnAccelFilter = new TorqueFilter(25);
@@ -48,7 +54,7 @@ public class DriverInput extends Input {
         } else {
             stingerRetractSpeed = 0.0;
         }
-        
+
         calcIntake();
         calcDrivebase();
     }
@@ -73,7 +79,61 @@ public class DriverInput extends Input {
     }
 
     private void calcElevator() {
+        if (operator.getAButton()) {
+            autoStack = true;
+        }
+        if (operator.getXButton()) {
+            autoStack = false;
+            elevatorPosition = feedback.getElevatorHeight();
+        }
+        if (autoStack) {
+            if (feedback.isElevatorHere(Constants.autoStackLevel.getDouble())) {
+                elevatorPosition = Constants.FloorElevatorLevel2.getDouble();
+                newPosition = true;
+                wentDown = true;
 
+                numTotes++;
+            } else if (!wentDown) {
+                elevatorPosition = Constants.autoStackLevel.getDouble();
+                newPosition = true;
+            } else if (wentDown && feedback.isElevatorHere(Constants.FloorElevatorLevel2.getDouble())) {
+                wentDown = false;
+                autoStack = false;
+            }
+        } else if (operator.getRightTrigger()) {
+            newPosition = true;
+            elevatorPosition = Constants.PlaceLevel1.getDouble();
+            if (feedback.isElevatorDone()) {
+                armOpen = true;
+            } else {
+                armOpen = false;
+            }
+        } else {
+            if (panel.getLevel1Button()) {
+                elevatorPosition = Constants.FloorElevatorLevel1.getDouble();
+                newPosition = true;
+            } else if (panel.getLevel2Button() || operator.getBButton()) {
+                elevatorPosition = Constants.FloorElevatorLevel2.getDouble();
+                newPosition = true;
+            } else if (panel.getLevel3Button()) {
+                elevatorPosition = Constants.FloorElevatorLevel3.getDouble();
+                newPosition = true;
+            } else if (panel.getLevel4Button()) {
+                elevatorPosition = Constants.FloorElevatorLevel4.getDouble();
+                newPosition = true;
+            } else if (panel.getLevel5Button()) {
+                elevatorPosition = Constants.FloorElevatorLevel5.getDouble();
+                newPosition = true;
+            } else if (panel.getLevel6Button()) {
+                elevatorPosition = Constants.FloorElevatorLevel6.getDouble();
+                newPosition = true;
+            } else if (operator.getYButton()) {
+                elevatorPosition = Constants.transportLevel.getDouble();
+                newPosition = true;
+            } else {
+                newPosition = false;
+            }
+        }
     }
 
     private void calcOverride() {
