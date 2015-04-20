7886b7ca3b6feddada183d5427550cdbac90d48d
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-04-20 17:20:48 -0500

    Remove old auto modes

--- src/org/texastorque/texastorque2015/auto/AutoPicker.java
+++ src/org/texastorque/texastorque2015/auto/AutoPicker.java
@@ -4,10 +4,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoPicker {
 
-    public final static byte TEST_AUTO = 100;
     public final static byte DEFAULT_AUTO = 0;
     public final static byte DRIVE_AUTO = 1;
-    public final static byte TAKE_AUTO = 2;
+    public final static byte CAN_AUTO = 4;
 
     public static void init() {
         SmartDashboard.putNumber("AutoMode", DEFAULT_AUTO);
@@ -20,13 +19,7 @@ public class AutoPicker {
             return new DoNothingAuto();
         } else if (mode == DRIVE_AUTO) {
             return new DriveAuto();
-        } else if (mode == TEST_AUTO) {
-            return new TestAuto();
-        } else if (mode == TAKE_AUTO) {
-            return new TakeSomethingAuto();
-        } else if (mode == 3) {
-            return new YellowToteAuto();
-        } else if (mode == 4) {
+        } else if (mode == CAN_AUTO) {
             return new TwoCanGrabber();
         }
         return new DoNothingAuto();
